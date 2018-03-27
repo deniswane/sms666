f36c617d1027af6569479f3e7ae85f41ece7d350
Kwok <guojam@outlook.com>
2018-03-27 09:02:49 +0000

    add confirm text

--- database/seeds/SmsContentTableSeeder.php
+++ database/seeds/SmsContentTableSeeder.php
@@ -11,9 +11,11 @@ class SmsContentTableSeeder extends Seeder
      */
     public function run()
     {
+        $phone = ['1','2','3','4','5','6','7','8','9','10'];
+        $faker = app(Faker\Generator::class);
 
-        $sms_contents = factory(App\Models\SmsContent::class)->times(100)->make()->each(function ($status) {
-            $status->phone_number_id = 2;
+        $sms_contents = factory(App\Models\SmsContent::class)->times(100)->make()->each(function ($status) use ($faker, $phone) {
+            $status->phone_number_id = $faker->randomElement($phone);
         });
 
         App\Models\SmsContent::insert($sms_contents->toArray());
