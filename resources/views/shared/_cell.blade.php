68b120dd8a641183ae6e771b2c7b956aeaa5c51f
Kwok <guojam@outlook.com>
2018-03-27 07:59:11 +0000

    fix error when data is null

--- resources/views/shared/_cell.blade.php
+++ resources/views/shared/_cell.blade.php
@@ -1,16 +1,19 @@
 {{--每个国家号码--}}
-@foreach($numbers as $num)
-    <div class="Cell">
-        <div>{{$num->country}}<br>
-            {{--号码-国家图片-国家--}}
-            <a href="{{ route('phone.detail',$num->id) }}">
-                <img src="{{ asset("img/Latvia.jpg") }}" alt="SMS - {{ $num->country }}" style="vertical-align: middle;">&nbsp;&nbsp;{{$num->phone}}<br>
-            </a>
-            <strong> SMS received:{{ $num->amount }}
-                <section
-                        style="border:none; height: auto; padding: 1px; width: auto; background: #33FF66;">
-                </section>
-            </strong>
+@if(count($numbers)>0)
+    @foreach($numbers as $num)
+        <div class="Cell">
+            <div>{{$num->country}}<br>
+                {{--号码-国家图片-国家--}}
+                <a href="{{ route('phone.detail',$num->id) }}">
+                    <img src="{{ asset("img/Latvia.jpg") }}" alt="SMS - {{ $num->country }}"
+                         style="vertical-align: middle;">&nbsp;&nbsp;{{$num->phone}}<br>
+                </a>
+                <strong> SMS received:{{ $num->amount }}
+                    <section
+                            style="border:none; height: auto; padding: 1px; width: auto; background: #33FF66;">
+                    </section>
+                </strong>
+            </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+@endif
