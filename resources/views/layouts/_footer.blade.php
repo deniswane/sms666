f9cc8fa977f351c079b63d553b2ae76f2316bc4a
liu <13113199200@163.com>
2018-05-10 03:16:03 +0000

    change _fotter detail

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,5 @@
-<div id="bottom_footer" align="center"><a href="/">Receive-sms-online.info</a> | <a href="privacy.php">Privacy
-        Policy</a> | Copyright &copy; 2018 <a target="_blank" href="https://payeer.com"><img
-                style="vertical-align: middle;" src="/img/payeer-logo.png" title="We accept Payeer" width="98"
-                height="42"/></a></div>
+<div id="bottom_footer" align="center"><a href="/">sms-receive-online</a> |  Copyright &copy; 2018
+        <a target="_blank" href="https://www.paypal.com/c2/webapps/mpp/home"><img
+                        style="vertical-align: middle;" src="/img/paypal-logo.svg" title="We accept Paypal" width="98"
+                        height="42"/></a></a>
+</div>
